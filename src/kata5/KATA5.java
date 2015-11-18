10bf12e0fbf27c909987baba3ee3d156cd9e9686
usuario <usuario@10.22.143.10>
2015-11-18 10:01:50 +0000

    Tabla mails con emails.txt

--- src/kata5/KATA5.java
+++ src/kata5/KATA5.java
@@ -1,5 +1,10 @@
 package kata5;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,7 +15,7 @@ import java.sql.Statement;
 
 public class KATA5 {
 
-      public static void main(String[] args) throws SQLException, ClassNotFoundException {
+      public static void main(String[] args) throws SQLException, ClassNotFoundException, FileNotFoundException, IOException {
         //Class.forName("oracle.jdbc.driver.OracleDriver");
         //Connection conecta = DriverManager.getConnection("jdbc:oracle:thin:@10.22.143.90:1521:orcl", "system","orcl");
         Class.forName("org.sqlite.JDBC");
@@ -22,7 +27,20 @@ public class KATA5 {
             System.out.println("ID = "+resultado.getInt("ID"));
             System.out.println("NAME = "+resultado.getString("NAME"));
         }
+          String nameFile = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\KATA5\\data\\emails.txt";
         
+          BufferedReader reader = new BufferedReader(new FileReader(new File (nameFile)));
+          String mail;
+          while((mail = reader.readLine())!=null){
+              if(!mail.contains("@"))continue;
+              query = "INSERT INTO MAILS (mail)VALUES('"+mail+"')";
+              state.executeUpdate(query);
+          }
+          
+          
+          resultado.close();
+          state.close();
+          conecta.close();
     }
     
 }
